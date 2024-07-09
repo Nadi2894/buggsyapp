fd8fb3ad0a76c756bad460dd5f08c732379767bf
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-07-09 12:46:19 +0200

    React is set up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Buggsyapp from "./Buggsyapp";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>hello</h1>
+        <Buggsyapp />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Buggsyapp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Buggsyapp() {
+  return <h1> haha</h1>;
+}
