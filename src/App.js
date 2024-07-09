6a4c856925a91d2884fbfab77e6307e259b7eff3
Nadi2894 <nadirabegimnadyrbekova@gmail.com>
2024-07-09 17:44:34 +0200

    Logo added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>hello</h1>
         <Buggsyapp />
       </header>
     </div>
--- src/Buggsyapp.js
+++ src/Buggsyapp.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./Buggsyapp.css";
+import Mylogo from "./images/Mylogo.jpg";
 
 export default function Buggsyapp() {
-  return <h1> haha</h1>;
+  return (
+    <div className="container">
+      <ul>
+        <img src={Mylogo} alt="" width={150} />
+        <li>Tickets</li>
+        <li>New tickets</li>
+        <li>Reports</li>
+      </ul>
+    </div>
+  );
 }
